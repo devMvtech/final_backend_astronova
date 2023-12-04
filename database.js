2a2d32d19349350b43faaefba8740dd510e31762
mohit <mv386631@gmail.com>
2023-12-04 17:56:53 +0530

    first commit

--- database.js
+++ database.js
@@ -2,7 +2,10 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
-
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connect to PostgreSQL successfully!");
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const { PORT, CLIENT_URL } = require("./src/constants");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const cors = require("cors");
+// const path = require("path");
+
+// import passport middleware
+require("./src/middlewares/passport-middleware");
+
+// initialize middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(passport.initialize());
+
+// import routes
+const donatorRoutes = require("./src/routes/donator_auth");
+const embassadorRoutes = require("./src/routes/embassador");
+
+// initialize routes
+app.use("/api/donator", donatorRoutes);
+app.use("/api/embassador", embassadorRoutes);
+app.use("/", (req, res) => {
+  console.log("Welcome to astronova");
+});
+
+// app start
+const appStart = () => {
+  try {
+    app.listen(PORT, () => {
+      SERVER_URL: process.env.SERVER_URL,
+        console.log(`The app is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error : ${error.message}`);
+  }
+};
+
+appStart();
