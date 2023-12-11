0b69efe6e76221f137b5a1d21e6ffe33cb7466d7
mohit <mv386631@gmail.com>
2023-12-11 17:28:18 +0530

    fix 2

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ require("./src/middlewares/passport-middleware");
 // initialize middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(passport.initialize());
 
 // import routes
