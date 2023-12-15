6ee06bf3f5ddf95a02cb54111f896a1e21efd2e3
mohit <mv386631@gmail.com>
2023-12-15 11:51:32 +0530

    increase file limit

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const cors = require("cors");
 // import passport middleware
 require("./src/middlewares/passport-middleware");
 
+// Increase payload limit
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 // initialize middlewares
 app.use(express.json());
 app.use(cookieParser());
