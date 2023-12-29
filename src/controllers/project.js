3eafc4d81ef9cd076491e73107704278b9ecd459
mohit <mv386631@gmail.com>
2023-12-29 22:48:17 +0530

    Add projects API

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -2,6 +2,9 @@ const db = require("../../database.js");
 const fs = require("fs");
 
 // Create Project
+
+// Create a new project with members
+
 exports.createProject = async (req, res) => {
   const {
     cover_img,
@@ -12,16 +15,26 @@ exports.createProject = async (req, res) => {
     product_img,
     status,
     admin_id,
-    team_members,
+    member_count,
+    membersData,
   } = req.body;
 
   try {
-    // Start a transaction
-    await db.query("BEGIN");
+    // Check if the title already exists in the projects table
+    const titleExists = await db.query(
+      "SELECT * FROM projects WHERE title = $1",
+      [title]
+    );
 
-    // Insert project details
+    if (titleExists.rows.length > 0) {
+      return res.status(400).json({
+        error: "Project with the provided title already exists.",
+      });
+    }
+
+    // Separate file upload logic from project creation
     const projectResult = await db.query(
-      "INSERT INTO projects (cover_img, title, subtitle, short_description, long_description, product_img, status, admin_id, team_members) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+      `INSERT INTO projects (cover_img, title, subtitle, short_description, long_description, product_img, status, admin_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`,
       [
         cover_img,
         title,
@@ -31,33 +44,50 @@ exports.createProject = async (req, res) => {
         product_img,
         status,
         admin_id,
-        team_members,
       ]
     );
 
     const projectId = projectResult.rows[0].id;
 
-    // Insert team members associated with the project into the junction table
-    if (team_members && team_members.length > 0) {
-      const insertTeamMembersQuery =
-        "INSERT INTO project_team_members (project_id, member_id) VALUES ($1, $2)";
+    // Create members and associate them with the project
+    const createdMembers = [];
+    for (let i = 0; i < member_count; i++) {
+      const memberData = membersData[i] || {};
+      const memberResult = await db.query(
+        "INSERT INTO members (profile_img, name, position) VALUES ($1, $2, $3) RETURNING member_id",
+        [memberData.profile_img, memberData.name, memberData.position]
+      );
 
-      for (const teamMemberId of team_members) {
-        await db.query(insertTeamMembersQuery, [projectId, teamMemberId]);
-      }
+      const memberId = memberResult.rows[0].member_id;
+
+      await db.query(
+        "INSERT INTO project_members (project_id, member_id) VALUES ($1, $2)",
+        [projectId, memberId]
+      );
+
+      createdMembers.push({ member_id: memberId, ...memberData });
     }
 
-    // Commit the transaction
-    await db.query("COMMIT");
-
-    return res
-      .status(201)
-      .json({ success: true, project: projectResult.rows[0] });
+    return res.status(201).json({
+      project: {
+        id: projectId,
+        cover_img,
+        title,
+        subtitle,
+        short_description,
+        long_description,
+        product_img,
+        status,
+        admin_id,
+      },
+      members: createdMembers,
+    });
   } catch (error) {
-    // Rollback the transaction in case of an error
-    await db.query("ROLLBACK");
-    console.error(error.message);
-    return res.status(500).json({ error: error.message });
+    console.error(error);
+
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
   }
 };
 
@@ -135,33 +165,105 @@ exports.updateProject = async (req, res) => {
 exports.getAllProjects = async (req, res) => {
   try {
     const projects = await db.query(
-      "SELECT id, title, team_members FROM projects"
+      "SELECT projects.id, projects.cover_img, projects.title, projects.subtitle, projects.short_description, projects.long_description, projects.product_img, projects.status, projects.admin_id, members.member_id, members.profile_img AS member_profile_img, members.name AS member_name, members.position AS member_position FROM projects LEFT JOIN project_members ON projects.id = project_members.project_id LEFT JOIN members ON project_members.member_id = members.member_id"
     );
 
-    // Parse the JSONB array in each project
-    const projectsWithParsedTeamMembers = projects.rows.map((project) => ({
-      id: project.id,
-      title: project.title,
-      team_members: JSON.parse(project.team_members),
-    }));
+    // Organize the data into a structured format
+    const formattedProjects = [];
+    let currentProject = null;
 
-    return res
-      .status(200)
-      .json({ success: true, projects: projectsWithParsedTeamMembers });
+    for (const project of projects.rows) {
+      if (!currentProject || currentProject.id !== project.id) {
+        // New project
+        currentProject = {
+          id: project.id,
+          cover_img: project.cover_img,
+          title: project.title,
+          subtitle: project.subtitle,
+          short_description: project.short_description,
+          long_description: project.long_description,
+          product_img: project.product_img,
+          status: project.status,
+          admin_id: project.admin_id,
+          members: [],
+        };
+        formattedProjects.push(currentProject);
+      }
+
+      // Add member data to the current project
+      if (project.member_id) {
+        currentProject.members.push({
+          member_id: project.member_id,
+          profile_img: project.member_profile_img,
+          name: project.member_name,
+          position: project.member_position,
+        });
+      }
+    }
+
+    return res.status(200).json({
+      success: true,
+      projects: formattedProjects,
+    });
   } catch (error) {
     console.error(error.message);
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
   }
 };
 
 // Get Project by ID
 exports.getProjectById = async (req, res) => {
+  const projectId = req.params.id; // Assuming the project ID is passed as a route parameter
+
   try {
-    const { id } = req.params;
-    const project = await db.query("SELECT * FROM project WHERE id = $1", [id]);
-    res.json(project.rows[0]);
-  } catch (err) {
-    console.log(err.message);
+    const projectData = await db.query(
+      "SELECT projects.id, projects.cover_img, projects.title, projects.subtitle, projects.short_description, projects.long_description, projects.product_img, projects.status, projects.admin_id, members.member_id, members.profile_img AS member_profile_img, members.name AS member_name, members.position AS member_position FROM projects LEFT JOIN project_members ON projects.id = project_members.project_id LEFT JOIN members ON project_members.member_id = members.member_id WHERE projects.id = $1",
+      [projectId]
+    );
+
+    if (projectData.rows.length === 0) {
+      return res.status(404).json({
+        error: "Project not found",
+      });
+    }
+
+    // Organize the data into a structured format
+    const formattedProject = {
+      id: projectData.rows[0].id,
+      cover_img: projectData.rows[0].cover_img,
+      title: projectData.rows[0].title,
+      subtitle: projectData.rows[0].subtitle,
+      short_description: projectData.rows[0].short_description,
+      long_description: projectData.rows[0].long_description,
+      product_img: projectData.rows[0].product_img,
+      status: projectData.rows[0].status,
+      admin_id: projectData.rows[0].admin_id,
+      members: [],
+    };
+
+    // Add member data to the current project
+    projectData.rows.forEach((project) => {
+      if (project.member_id) {
+        formattedProject.members.push({
+          member_id: project.member_id,
+          profile_img: project.member_profile_img,
+          name: project.member_name,
+          position: project.member_position,
+        });
+      }
+    });
+
+    return res.status(200).json({
+      success: true,
+      project: formattedProject,
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
   }
 };
 
